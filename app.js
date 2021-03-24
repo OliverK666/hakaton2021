b000e29a4cd2089ee57e07da7fefcbe3b1c0e218
Aleksa Siriski <aleksasiriski@lavelektronik.com>
2021-03-24 15:22:24 +0100

    Dodat REST

--- app.js
+++ app.js
@@ -1,7 +1,57 @@
 var express = require("express");
-var serveStatic = require("serve-static");
-
+var bodyParser = require("body-parser");
+var items = [];
+var router = express.Router();
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
+router.use(bodyParser.json());
+router.route("/")
+    .get(function(req, res, next) {
+        res.send({
+            status: "Items found",
+            items: items
+        });
+    })
+    .post(function(req, res, next) {
+        items.push(req.body);
+        res.send({
+            status: "Item added",
+            itemId: items.length - 1
+        });
+    })
+    .put(function(req, res, next) {
+        items = req.body;
+        res.send({
+            status: "Items replaced"
+        });
+    })
+    .delete(function(req, res, next) {
+        items = [];
+        res.send({
+            status: "Items cleared"
+        });
+    });
+router.router("/:id")
+    .get(function(req, res, next) {
+        var id = req.params.id;
+        if (id && items[Number(id)]) {
+            res.send({
+                status: "Item found",
+                item: items[Number(id)]
+            });
+        } else {
+            res.status(404).send({
+                status: "Not found"
+            });
+        }
+    })
+    .all(function(req, res, next) {
+        res.status(501).send({
+            status: "Not implemented"
+        })
+    });
 var app = express()
-    .use(serveStatic(__dirname + "/front"))
+    .use("/items", router)
     .listen(3000);
-console.log("Server is listening on port 3000");
+console.log("Server started and listeting on port 3000");
